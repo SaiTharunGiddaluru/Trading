3dcc915c38be8d8af1cd47a2fff8be1d095d27d0
SaiTharunGiddaluru <saitharun.00005@gmail.com>
2024-09-09 10:46:10 +0530

    changes to the anchor tags

--- frontend/src/landing_page/products/LeftSection.js
+++ frontend/src/landing_page/products/LeftSection.js
@@ -13,7 +13,7 @@ function LeftSection({
       <div className="container">
         <div className="row">
           <div className="col-6 ">
-            <img src={imgUrl} />
+            <img src={imgUrl} alt="newmedia"/>
           </div>
           <div className="col-6 p-5 "  >
             <h1>{productname}</h1>
@@ -22,12 +22,11 @@ function LeftSection({
             </p>
             <div className="mb-5">
             <a style={{textDecoration:"none"}}href="linkss"> Try demo <i class="fa-solid fa-arrow-right"></i></a>
-            <a style={{textDecoration:"none"}}className="mx-5" href="sjjsj"> learnMore <i class="fa-solid fa-arrow-right"></i></a>
-
+            <a style={{textDecoration:"none"}}className="mx-5" href="linkings"> learnMore <i class="fa-solid fa-arrow-right"></i></a>
             </div>
            <div>
-           <a href="somelinks" style={{textDecoration:"none"}}> <img src={Google}/> </a>
-           <a className="mx-3" href="somelinks" style={{textDecoration:"none"}}> <img src={ Appstore} /></a>
+           <a href="somelinks" style={{textDecoration:"none"}}> <img src={Google} alt="newmedia"/> </a>
+           <a className="mx-3" href="somelinks" style={{textDecoration:"none"}}> <img src={ Appstore} alt="newmedia"/></a>
            </div>
            
           </div>
